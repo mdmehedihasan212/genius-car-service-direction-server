3abbd3b1377730f469b313d5eb257b1169c13d31
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-22 13:00:16 +0600

    add get user and dynamic params set user

--- index.js
+++ index.js
@@ -12,15 +12,40 @@ app.get('/', (req, res) => {
     res.send("Hello Mehedi!")
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const res = require("express/lib/response");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.drus2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const servicesCollection = client.db('carServices').collection('users');
+
+        // find all services (find multiple documents)
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // set dynamic params (find one documents)
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const services = await servicesCollection.findOne(query);
+            res.send(services);
+        })
+
+
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
 
 
 app.listen(port, () => {
